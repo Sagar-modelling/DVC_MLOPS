28d1d53854d6c15d57bc78afa40954d359cede24
sagariit.kanpur1@gmail.com <sagariit.kanpur1@gmail.com>
2022-03-10 15:17:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Commands Used
+
+```bash
+conda create -n wineq python==3.7 -y
+conda activate <env name> 
+touch requirements.txt
+pip install -r requirements.txt
+touch README.md
+
+git init
+dvc init
+dvc add data_given/winequality.csv
+```
---
+++ template.py
@@ -0,0 +1,27 @@
+import os
+
+
+dirs = [
+    os.path.join("data","raw"), #to create multiple folders
+    os.path.join("data","processed"),
+    "notebooks", #creating single folder
+    "saved_models",
+    "src" #source folder where we do development
+]
+
+for dir_ in dirs: #createing the directory structure
+    os.makedirs(dir_, exist_ok=True)
+#The purpose of the . gitkeep file is to solve problem of Git not pushing empty folders to remote DVCS repos like GitHub or GitLab.
+    with open(os.path.join(dir_, ".gitkeep"), "w") as f: 
+        pass
+
+files = [                   #files
+        "dvc.yaml",
+        "params.yaml",
+        ".gitignore",
+        os.path.join("src","__init__.py")
+]
+
+for file_ in files:
+    with open(file_, "w") as f:
+        pass
